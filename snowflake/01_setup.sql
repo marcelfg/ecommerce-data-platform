731b4165958a9724c0d96833f88e56ca76bcb3b9
Marcel Fellipe Guimaraes <marcel2.guimaraes@live.uwe.ac.uk>
2026-06-11 13:01:33 +0100

    Adds snowflake setup script to create database and schemas

---
+++ snowflake/01_setup.sql
@@ -0,0 +1,8 @@
+-- Create the database
+CREATE DATABASE olist;
+
+-- Create the schemas
+CREATE SCHEMA olist.raw;
+CREATE SCHEMA olist.staging;
+CREATE SCHEMA olist.intermediate;
+CREATE SCHEMA olist.marts;
