65e66a290d4a8175d68126a858e698ea77836a90
Marcel Fellipe Guimaraes <marcel2.guimaraes@live.uwe.ac.uk>
2026-06-12 12:04:06 +0100

    Creates snowflake raw dml files

---
+++ snowflake/raw/dml/geolocation.sql
@@ -0,0 +1,16 @@
+COPY INTO olist.raw.geolocation
+FROM @olist.raw.s3_olist_stage/olist_geolocation_dataset.csv
+FILE_FORMAT = (
+    TYPE = 'CSV'
+    FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+    PARSE_HEADER = TRUE
+    ERROR_ON_COLUMN_COUNT_MISMATCH = FALSE
+)
+MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE
+INCLUDE_METADATA = (
+    _loaded_at = METADATA$START_SCAN_TIME,
+    _source_file = METADATA$FILENAME,
+    _file_row_number = METADATA$FILE_ROW_NUMBER,
+    _file_last_modified = METADATA$FILE_LAST_MODIFIED
+)
+ON_ERROR = 'CONTINUE';
