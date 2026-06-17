eaf78e4d3f984255702c86619260f17193aea3af
Marcel Fellipe Guimaraes <marcel2.guimaraes@live.uwe.ac.uk>
2026-06-17 17:02:05 +0100

    Adds stg_olist__customers model with tests and sources

---
+++ dbt/models/staging/stg_olist__customers.sql
@@ -0,0 +1,20 @@
+WITH source AS (
+
+    SELECT * FROM {{ source('raw', 'customers') }}
+
+),
+
+renamed AS (
+
+    SELECT
+        customer_id,
+        customer_unique_id,
+        customer_zip_code_prefix AS zip_code,
+        customer_city AS city,
+        customer_state AS state,
+        _loaded_at::TIMESTAMP_NTZ AS _loaded_at
+    FROM source
+
+)
+
+SELECT * FROM renamed
