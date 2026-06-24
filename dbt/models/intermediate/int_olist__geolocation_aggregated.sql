a507fc457b268c7052c34649b9c1629920bde283
Marcel Fellipe Guimaraes <marcel2.guimaraes@live.uwe.ac.uk>
2026-06-24 13:30:06 +0100

    Updates int geolocation_aggregated model logic

--- dbt/models/intermediate/int_olist__geolocation_aggregated.sql
+++ dbt/models/intermediate/int_olist__geolocation_aggregated.sql
@@ -4,17 +4,36 @@ WITH geolocation AS (
 
 ),
 
-exact_deduped AS (
+standardization AS (
+
+    SELECT * FROM {{ ref('location_standardization') }}
+
+),
+
+cleaned AS (
 
     SELECT
+        geolocation.zip_code,
+        geolocation.latitude,
+        geolocation.longitude,
+        COALESCE(standardization.standardized_city, geolocation.city) AS city,
+        COALESCE(standardization.standardized_state, geolocation.state) AS state
+    FROM geolocation
+    LEFT JOIN standardization
+        ON LOWER(TRIM(geolocation.city)) = LOWER(TRIM(standardization.raw_city))
+        AND LOWER(TRIM(geolocation.state)) = LOWER(TRIM(standardization.raw_state))
+
+),
+
+deduplicated AS (
+
+    SELECT DISTINCT
         zip_code,
-        latitude,
-        longitude,
         city,
         state,
-        MAX(_loaded_at) AS _loaded_at
-    FROM geolocation
-    GROUP BY zip_code, latitude, longitude, city, state
+        latitude,
+        longitude
+    FROM cleaned
 
 ),
 
@@ -22,10 +41,17 @@ aggregated AS (
 
     SELECT
         zip_code,
-        AVG(latitude)::NUMBER(8,6)  AS latitude,
-        AVG(longitude)::NUMBER(9,6) AS longitude
-    FROM exact_deduped
-    GROUP BY zip_code
+        city,
+        state,
+        AVG(latitude) AS latitude,
+        AVG(longitude) AS longitude
+    FROM deduplicated
+    GROUP BY zip_code, city, state
+    -- To handle cases where a zip code is incorrectly associated with a city.
+    QUALIFY ROW_NUMBER() OVER (
+        PARTITION BY zip_code
+        ORDER BY COUNT(*) DESC
+    ) = 1
 
 )
 
